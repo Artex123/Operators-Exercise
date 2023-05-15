48640986b6336484d5b70476b5d9e20ed9782d31
Ben Zellers <zellersdenver@gmail.com>
2023-05-15 10:38:42 -0400

    I did it!

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} = {quotient} remainder {remainder}");
+            if (a == 17 && b == 4);
+            
+            //Console.WriteLine(a + "/" + b + " is " + quotient + " remainder " + remainder);
+
+            Console.WriteLine("What is the radius of your circle");
+            var radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}");
+           
+            
+        
+        }
+        public static double AreaOfCircle(double radius)
+        {
+           return Math.PI * Math.Pow(radius, 2); 
+           
         }
     }
 }
